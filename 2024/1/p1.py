27c7ff4e5645ea58014b625e2b9c25593d808eaa
Armaan Desai <armaandesayuno@gmail.com>
2024-12-01 14:40:22 -0600

    added day 1 solutions

--- 2024/1/p1.py
+++ 2024/1/p1.py
@@ -5,3 +5,18 @@ input_file_path = os.path.join(current_dir, "input.txt")
 
 with open(input_file_path, "r") as file:
     lines = file.readlines()
+
+l1 = []
+l2 = []
+sum = 0
+for line in lines:
+    v1, v2 = line.split("   ")
+    l1.append(int(v1))
+    l2.append(int(v2))
+l1.sort()
+l2.sort()
+
+for v1, v2 in zip(l1, l2):
+    sum += abs(v1 - v2)
+
+print(sum)
--- 2024/1/p2.py
+++ 2024/1/p2.py
@@ -5,3 +5,20 @@ input_file_path = os.path.join(current_dir, "input.txt")
 
 with open(input_file_path, "r") as file:
     lines = file.readlines()
+
+l1 = []
+l2 = {}
+sum = 0
+for line in lines:
+    v1, v2 = line.split("   ")
+    l1.append(int(v1))
+    if int(v2) in l2:
+        l2[int(v2)] += 1
+    else:
+        l2[int(v2)] = 1
+
+for v1 in l1:
+    if v1 in l2:
+        sum += v1 * l2[v1]
+
+print(sum)
