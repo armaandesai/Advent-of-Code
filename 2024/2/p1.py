292b8eeb95e3f7093fc0e0980462bc5a67ad933a
Armaan Desai <armaandesayuno@gmail.com>
2024-12-01 23:55:48 -0600

    day 2

--- 2024/2/p1.py
+++ 2024/2/p1.py
@@ -5,3 +5,33 @@ input_file_path = os.path.join(current_dir, "input.txt")
 
 with open(input_file_path, "r") as file:
     lines = file.readlines()
+count = 0
+total = []
+for line in lines:
+    total.append(list(map(int, line.split())))
+
+for vals in total:
+    #check increasing or decreasing
+    inc = True
+    for i in range(1, len(vals)):
+        if vals[i] < vals[i - 1]:
+            inc = False
+            break
+    dec = True
+    for i in range(1, len(vals)):
+        if vals[i] > vals[i - 1]:
+            dec = False
+            break
+    if not inc and not dec:
+        continue
+    
+    #check if the list is valid
+    check = True
+    for i in range(1, len(vals)):
+        diff = abs(vals[i] - vals[i - 1])
+        if diff > 3 or diff < 1:
+            check = False
+            break
+    if check:
+        count += 1
+print(count)
--- 2024/2/p2.py
+++ 2024/2/p2.py
@@ -5,3 +5,42 @@ input_file_path = os.path.join(current_dir, "input.txt")
 
 with open(input_file_path, "r") as file:
     lines = file.readlines()
+count = 0
+total = []
+for line in lines:
+    total.append(list(map(int, line.split())))
+
+def valid(vals):
+    #check increasing or decreasing
+    inc = True
+    for i in range(1, len(vals)):
+        if vals[i] < vals[i - 1]:
+            inc = False
+            break
+    dec = True
+    for i in range(1, len(vals)):
+        if vals[i] > vals[i - 1]:
+            dec = False
+            break
+    if not inc and not dec:
+        return False
+    
+    #check if the list is valid
+    for i in range(1, len(vals)):
+        diff = abs(vals[i] - vals[i - 1])
+        if diff > 3 or diff < 1:
+            return False
+    return True
+
+for vals in total:
+    check = valid(vals)
+    if check:
+        count += 1
+    else:
+        for i in range(len(vals)):
+            substring = vals[:i] + vals[i + 1:]
+            if valid(substring):
+                count += 1
+                break
+print(count)
+
