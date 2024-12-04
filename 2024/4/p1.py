1e2a239346c9a79add854973592aea2a458575f0
Armaan Desai <armaandesayuno@gmail.com>
2024-12-03 23:36:09 -0600

    day 4

--- 2024/4/p1.py
+++ 2024/4/p1.py
@@ -5,3 +5,38 @@ input_file_path = os.path.join(current_dir, "input.txt")
 
 with open(input_file_path, "r") as file:
     lines = file.readlines()
+
+def find(str, i, j, direction):
+    if len(str) == 0:
+        global total
+        total += 1
+        return
+    x, y = directions[direction]
+    if i + x < 0 or i + x >= len(matrix) or j + y < 0 or j + y >= len(matrix[i]) or str[:1] != matrix[i + x][j + y]:
+        return
+    find(str[1:], i + x, j + y, direction)
+
+total = 0
+
+matrix = []
+for line in lines:
+    matrix.append(list(line.strip()))
+
+directions = {
+    1: [0, 1],
+    2: [0, -1],
+    3: [1, 0],
+    4: [-1, 0],
+    5: [1, 1],
+    6: [-1, -1],
+    7: [1, -1],
+    8: [-1, 1]
+}
+for i in range(len(matrix)):
+    for j in range(len(matrix[i])):
+        if matrix[i][j] == "X":
+            for k in range(1, 9):
+                find("MAS", i, j, k)
+            
+
+print(total)
--- 2024/4/p2.py
+++ 2024/4/p2.py
@@ -5,3 +5,29 @@ input_file_path = os.path.join(current_dir, "input.txt")
 
 with open(input_file_path, "r") as file:
     lines = file.readlines()
+
+def find(i, j):
+    global total
+    if matrix[i - 1][j - 1] == "M" and matrix[i - 1][j + 1] == "M" and matrix[i + 1][j - 1] == "S" and matrix[i + 1][j + 1] == "S":
+        total += 1
+    elif matrix[i - 1][j - 1] == "M" and matrix[i - 1][j + 1] == "S" and matrix[i + 1][j - 1] == "M" and matrix[i + 1][j + 1] == "S":
+        total += 1
+    elif matrix[i - 1][j - 1] == "S" and matrix[i - 1][j + 1] == "S" and matrix[i + 1][j - 1] == "M" and matrix[i + 1][j + 1] == "M":
+        total += 1
+    elif matrix[i - 1][j - 1] == "S" and matrix[i - 1][j + 1] == "M" and matrix[i + 1][j - 1] == "S" and matrix[i + 1][j + 1] == "M":
+        total += 1
+    
+
+total = 0
+
+matrix = []
+for line in lines:
+    matrix.append(list(line.strip()))
+
+for i in range(len(matrix)):
+    for j in range(len(matrix[i])):
+        if matrix[i][j] == "A" and i - 1 >= 0 and i + 1 < len(matrix) and j - 1 >= 0 and j + 1 < len(matrix[i]):
+            find(i, j)
+            
+
+print(total)
