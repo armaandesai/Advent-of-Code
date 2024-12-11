ab689b522e6cfe60d05b4630b9e99374ba89653f
Armaan Desai <armaandesayuno@gmail.com>
2024-12-11 00:17:40 -0600

    day 11

--- 2024/11/p1.py
+++ 2024/11/p1.py
@@ -5,3 +5,24 @@ input_file_path = os.path.join(current_dir, "input.txt")
 
 with open(input_file_path, "r") as file:
     lines = file.readlines()
+
+stones = list(map(int, lines[0].strip().split(" ")))
+
+for i in range(25):
+    length = len(stones)
+    i = 0
+    while i < length:
+        if stones[i] == 0:
+            stones[i] = 1
+        elif len(str(stones[i])) % 2 == 0:
+            first = str(stones[i])[:len(str(stones[i])) // 2]
+            second = str(stones[i])[len(str(stones[i])) // 2:]
+            stones[i] = int(first)
+            stones.insert(i + 1, int(second))
+            length += 1
+            i += 1
+        else:
+            stones[i] *= 2024
+        i += 1
+
+print(len(stones))
--- 2024/11/p2.py
+++ 2024/11/p2.py
@@ -5,3 +5,27 @@ input_file_path = os.path.join(current_dir, "input.txt")
 
 with open(input_file_path, "r") as file:
     lines = file.readlines()
+
+stones = list(map(int, lines[0].strip().split(" ")))
+cache = {}
+
+def blink(stone, n):
+    if n == 0:
+        return 1
+    if (stone, n) not in cache:
+        if stone == 0:
+            result = blink(1, n - 1)
+        elif len(str(stone)) % 2 == 0:
+            stone = str(stone)
+            result = 0
+            result += blink(int(stone[:len(stone) // 2]), n - 1)
+            result += blink(int(stone[len(stone) // 2:]), n - 1)
+        else:
+            result = blink(2024 * stone, n - 1)
+        cache[(stone, n)] = result
+    return cache[(stone, n)]
+
+res = 0
+for stone in stones:
+    res += blink(stone, 75)
+print(res)
