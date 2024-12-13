2e3192dfa9afa3a490b6d903333415f35bf0a134
armaandesai <armaandesayuno@gmail.com>
2024-12-13 02:20:53 -0500

    day 13

--- 2024/13/p1.py
+++ 2024/13/p1.py
@@ -1,7 +1,29 @@
 import os
+import re
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 input_file_path = os.path.join(current_dir, "input.txt")
 
 with open(input_file_path, "r") as file:
     lines = file.readlines()
+total = 0
+for line in lines:
+    if line.startswith("Button A"):
+        regex = re.findall(r'\+(\d+)', line)
+        a = list(map(int, regex))
+    elif line.startswith("Button B"):
+        regex = re.findall(r'\+(\d+)', line)
+        b = list(map(int, regex))
+    elif line.startswith("Prize"):
+        regex = re.findall(r'\=(\d+)', line)
+        p = list(map(int, regex))
+        # brute force
+        minimum = float("inf")
+        for i in range(1, 101):
+            for j in range(1, 101):
+                if i * a[0] + j * b[0] == p[0] and i * a[1] + j * b[1] == p[1]:
+                    if i * 3 + j < minimum:
+                        minimum = i * 3 + j
+        if minimum != float("inf"):
+            total += minimum
+print(total)
--- 2024/13/p2.py
+++ 2024/13/p2.py
@@ -1,7 +1,29 @@
 import os
+import re
+import numpy as np
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 input_file_path = os.path.join(current_dir, "input.txt")
 
 with open(input_file_path, "r") as file:
     lines = file.readlines()
+total = 0
+for line in lines:
+    if line.startswith("Button A"):
+        regex = re.findall(r'\+(\d+)', line)
+        a = list(map(int, regex))
+    elif line.startswith("Button B"):
+        regex = re.findall(r'\+(\d+)', line)
+        b = list(map(int, regex))
+    elif line.startswith("Prize"):
+        regex = re.findall(r'\=(\d+)', line)
+        p = [int(x) + 10000000000000 for x in regex]
+        # Ax = B
+        A = np.array([a, b]).T
+        B = np.array(p)
+        x = np.linalg.solve(A, B)
+        # correct for floating point errors
+        x = np.round(x, 2)
+        if np.all(np.mod(x, 1) == 0):
+            total += int(x[0] * 3 + x[1])
+print(total)
