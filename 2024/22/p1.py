ac1579b7bf080140750536b6253a08344168bb23
armaandesai <armaandesayuno@gmail.com>
2024-12-23 11:37:33 -0600

    day 22

--- 2024/22/p1.py
+++ 2024/22/p1.py
@@ -5,3 +5,21 @@ input_file_path = os.path.join(current_dir, "input.txt")
 
 with open(input_file_path, "r") as file:
     lines = file.readlines()
+
+vals = [int(line.strip()) for line in lines]
+total = 0
+for val in vals:
+    for i in range(2000):
+        res = val * 64
+        val = val ^ res
+        val %= 16777216
+
+        res = val // 32
+        val = val ^ res
+
+        res = val * 2048
+        val = val ^ res
+        val %= 16777216
+    total += val
+
+print(total)
