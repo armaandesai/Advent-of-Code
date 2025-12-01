8e081232ab340d30c6062ea6f76fcebdbcac3a20
Armaan Desai <armaandesayuno@gmail.com>
2025-11-30 23:19:01 -0600

    day 1

--- 2025/1/p1.py
+++ 2025/1/p1.py
@@ -5,3 +5,17 @@ input_file_path = os.path.join(current_dir, "input.txt")
 
 with open(input_file_path, "r") as file:
     lines = file.readlines()
+
+res = 0
+num = 50
+
+for line in lines:
+    mag = int(line[1:])
+    if line.startswith("L"):
+        num += mag
+    elif line.startswith("R"):
+        num -= mag
+    if num % 100 == 0:
+        res += 1
+        
+print(res)
--- 2025/1/p2.py
+++ 2025/1/p2.py
@@ -5,3 +5,21 @@ input_file_path = os.path.join(current_dir, "input.txt")
 
 with open(input_file_path, "r") as file:
     lines = file.readlines()
+
+res = 0
+num = 50
+
+for line in lines:
+    mag = int(line[1:])
+    for _ in range(mag):
+        if line.startswith('L'):
+            if num == 0:
+                num = 99
+            else:
+                num = (num - 1) % 100
+        else:
+            num = (num + 1) % 100
+        if num == 0:
+            res += 1
+        
+print(res)
