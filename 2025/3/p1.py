1fa4dec984786b363b3e1c0b389fbbb40a1a3904
Armaan Desai <armaandesayuno@gmail.com>
2025-12-02 23:20:45 -0600

    day 3

--- 2025/3/p1.py
+++ 2025/3/p1.py
@@ -5,3 +5,13 @@ input_file_path = os.path.join(current_dir, "input.txt")
 
 with open(input_file_path, "r") as file:
     lines = file.readlines()
+
+res = 0
+for line in lines:
+    bank = [int(x) for x in line.strip()]
+    curr = 0
+    for i in range(0, len(bank) - 1):
+        for j in range(i + 1, len(bank)):
+            curr = max(curr, bank[i] * 10 + bank[j])
+    res += curr
+print(res)
--- 2025/3/p2.py
+++ 2025/3/p2.py
@@ -5,3 +5,22 @@ input_file_path = os.path.join(current_dir, "input.txt")
 
 with open(input_file_path, "r") as file:
     lines = file.readlines()
+
+res = 0
+for line in lines:
+    bank = [int(x) for x in line.strip()]
+    digits = 12
+    curr = 0
+    i = 0
+    while digits > 0:
+        max = -1
+        idx = -1
+        for j in range(i, len(bank) - digits + 1):
+            if bank[j] > max:
+                max = bank[j]
+                idx = j
+        curr = curr * 10 + max
+        i = idx + 1
+        digits -= 1
+    res += curr
+print(res)
