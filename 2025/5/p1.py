0934c25c88cfc18f4b0b3ad2a82c4bfc7a97c578
Armaan Desai <armaandesayuno@gmail.com>
2025-12-04 23:23:26 -0600

    homework 5

--- 2025/5/p1.py
+++ 2025/5/p1.py
@@ -1,7 +1,33 @@
 import os
-
+from collections import defaultdict
 current_dir = os.path.dirname(os.path.abspath(__file__))
 input_file_path = os.path.join(current_dir, "input.txt")
 
 with open(input_file_path, "r") as file:
     lines = file.readlines()
+
+sep = False
+fresh = defaultdict(list)
+res = 0
+for line in lines:
+    line = line.strip()
+    if len(line) == 0:
+        sep = True
+        continue
+    if sep:
+        id = int(line)
+        check = False
+        for k in fresh:
+            if id < k:
+                continue
+            vals = fresh[k]
+            for val in vals:
+                if id <= val:
+                    check = True
+        if check:
+            res += 1
+    else:
+        start, stop = [int(val) for val in line.split("-")]
+        fresh[start].append(stop)
+
+print(res)
--- 2025/5/p2.py
+++ 2025/5/p2.py
@@ -1,7 +1,35 @@
 import os
-
+from collections import defaultdict
 current_dir = os.path.dirname(os.path.abspath(__file__))
 input_file_path = os.path.join(current_dir, "input.txt")
 
 with open(input_file_path, "r") as file:
     lines = file.readlines()
+
+fresh = []
+res = 0
+for line in lines:
+    line = line.strip()
+    if len(line) == 0:
+        break
+    start, stop = [int(val) for val in line.split("-")]    
+    fresh.append([start, stop])
+    
+fresh.sort(key=lambda x: x[0])
+output = []
+cur_start, cur_stop = fresh[0]
+for i in range(1, len(fresh)):
+    start, stop = fresh[i]
+    if start <= cur_stop:
+        cur_stop = max(cur_stop, stop)
+    else:
+        output.append([cur_start, cur_stop])
+        cur_start, cur_stop = start, stop   
+
+output.append([cur_start, cur_stop])
+
+res = 0
+for start, stop in output:
+    res += stop - start + 1
+
+print(res)
