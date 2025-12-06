b01c29bf1748aa10dcdf0c353b7f02bf9ff0f1d1
Armaan Desai <armaandesayuno@gmail.com>
2025-12-06 00:04:44 -0600

    day 6

--- 2025/6/p1.py
+++ 2025/6/p1.py
@@ -1,7 +1,31 @@
 import os
+import re
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 input_file_path = os.path.join(current_dir, "input.txt")
 
 with open(input_file_path, "r") as file:
     lines = file.readlines()
+
+matrix = []
+ops = None
+for line in lines:
+    line = line.strip()
+    line = re.sub(r'\s+', ' ', line)
+    line = line.split(" ")
+    if line[0].isdigit():
+        matrix.append([int(val) for val in line])
+    else:
+        ops = line
+res = 0
+for i, op in enumerate(ops):
+    if op == '*':
+        total = 1
+        for j in range(len(matrix)):
+            total *= matrix[j][i]
+    else:
+        total = 0
+        for j in range(len(matrix)):
+            total += matrix[j][i]
+    res += total
+print(res)
--- 2025/6/p2.py
+++ 2025/6/p2.py
@@ -1,7 +1,52 @@
 import os
+import re
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 input_file_path = os.path.join(current_dir, "input.txt")
 
 with open(input_file_path, "r") as file:
     lines = file.readlines()
+    
+matrix = []
+longest = -1
+for line in lines:
+    line = line.rstrip()
+    longest = max(longest, len(line))
+    matrix.append(line)
+    
+for i in range(len(matrix)):
+    while len(matrix[i]) < longest:
+        matrix[i] += " "
+
+symbol = None
+res = 0
+numbers = []
+for i in range(len(matrix[0])):
+    if all(matrix[j][i] == " " for j in range(len(matrix))):
+        if symbol == "+":
+            res += sum(numbers)
+        elif symbol == "*":
+            prod = 1
+            for n in numbers:
+                prod *= n
+            res += prod
+        numbers = []
+        continue
+    num = 0
+    for j in range(len(matrix) - 1):
+        if matrix[j][i] != " ":
+            num *= 10
+            num += int(matrix[j][i])
+    numbers.append(num)
+    if matrix[-1][i] != " ":
+        symbol = matrix[-1][i]
+
+if symbol == "+":
+    res += sum(numbers)
+elif symbol == "*":
+    prod = 1
+    for n in numbers:
+        prod *= n
+    res += prod
+    
+print(res)
